b808191b2764388f41a452a11ae1b504feae14bf
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-09-23 18:20:45 -0400

    eventlist/ eventlist.test/ app test
    added eventlist/ eventlist.test/ app test

---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import EventList from '../component/EventList';
+// import EventList from '../components/EventList';
+
+describe('<EventList /> component', () => {
+  test('has an element with "list" role', () => {
+    const EventListComponent = render(<EventList />);
+    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+  });
+});
---
+++ src/component/EventList.js
@@ -0,0 +1,13 @@
+// import './App.css';
+
+function EventList() {
+  return (
+    <div id="event-list">
+    <ul>
+     <li>event</li>
+      </ul>
+       </div>
+  );
+}
+
+export default EventList;
