1072b3045295d35fe207ac34a62586b4c914a3a4
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-09-22 22:34:10 -0400

    serverless & handler
    removed CALENDAR ID i have an error msg because im not using it
    added module exports get accesstoken

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,9 +1,8 @@
-'use strict';
+// 'use strict';
 
 const { google } = require("googleapis");
-const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
-const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const { CLIENT_SECRET, CLIENT_ID } = process.env; // Removed CALENDAR_ID since it's not used
 const redirect_uris = [
   "https://stefxmas.github.io/meet/"
 ];
@@ -14,12 +13,9 @@ const oAuth2Client = new google.auth.OAuth2(
   redirect_uris[0]
 );
 
+// Exporting the function to get the authorization URL
 module.exports.getAuthURL = async () => {
-  /**
-   *
-   * Scopes array is passed to the `scope` option. 
-   *
-   */
+  // Scopes array is passed to the `scope` option.
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -35,4 +31,34 @@ module.exports.getAuthURL = async () => {
       authUrl,
     }),
   };
+};
+
+// Exporting the function to get access token
+module.exports.getAccessToken = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  try {
+    // Exchange authorization code for access token
+    const { tokens } = await oAuth2Client.getToken(code);
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify(tokens),
+    };
+  } catch (error) {
+    // Handle error
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 };
