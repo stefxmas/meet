e9cc22ba1988f926f67329295b28190829381fda
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-09-30 18:48:31 -0400

    event.js

--- src/component/event.js
+++ src/component/event.js
@@ -1,6 +1,18 @@
-const Event = () => {
-    return (
-      <li></li>
+import { useState } from "react";
+
+const Event = ({event}) => {
+  const [showDetails, setshowDetails] = useState(false);  
+  return (
+      <li>
+<p>{event.summary}</p>
+<p>{event.location}</p>
+<p>{event.start?.dateTime}</p>
+<p>{event.end?.dateTime}</p>
+<button>onClick={() => setshowDetails(!showDetails)} {showDetails ? "Hide" : "Show"}</button>
+  {showDetails ? (
+   <p>{event.description}</p>     
+      ) : null}
+      </li>
     );
   }
   
