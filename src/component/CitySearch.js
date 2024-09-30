be6863ba2218f9daee48d68297f05c4763946069
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-09-30 18:01:03 -0400

    citySearch/eventList.test

--- src/component/CitySearch.js
+++ src/component/CitySearch.js
@@ -13,32 +13,36 @@ const CitySearch = ({ allLocations }) => {
         setQuery(value);
         setSuggestions(filteredLocations);
       };
-  
-    return (
-        <div id="city-search">
-        <input
-          type="text"
-          className="city"
-                  placeholder="Search for a city"
-          value={query}
-          onFocus={() => setShowSuggestions(true)}
-          onChange={handleInputChanged}
-        />
-        {showSuggestions ?
-        <ul className="suggestions">
-          {suggestions.map((suggestion) => {
-            return <li key={suggestion}>{suggestion}</li>
-          })}
-          <li key='See all cities'>
-            <b>See all cities</b>
-          </li>
-        </ul>
-        : null
-      }
-      </div>
-  )
-}
 
+      const handleItemClicked = (event) => {
+        const value = event.target.textContent;
+        setQuery(value);
+        setShowSuggestions(false); // to hide the list
+      };
+  
+      return (
+        <div id="city-search">
+          <input
+            type="text"
+            className="city"
+            placeholder="Search for a city"
+            value={query}
+            onFocus={() => setShowSuggestions(true)}
+            onChange={handleInputChanged}
+          />
+          {showSuggestions ?
+            <ul className="suggestions">
+              {suggestions.map((suggestion) => {
+                return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
+              })}
+              <li key='See all cities' onClick={handleItemClicked}>
+                <b>See all cities</b>
+              </li>
+            </ul>
+            : null
+          }
+        </div>
+     )}
 
 
 export default CitySearch;
