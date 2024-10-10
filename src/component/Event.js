4a926e34674aaa8080cff5e2e12ce7d0ba78d8d7
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-10-10 00:08:33 -0400

    event.js

--- src/component/Event.js
+++ src/component/Event.js
@@ -4,14 +4,19 @@ const Event = ({event}) => {
   const [showDetails, setshowDetails] = useState(false);  
   return (
       <li>
+        <div className="eventSummary">
 <p>{event.summary}</p>
 <p>{event.location}</p>
 <p>{event.start?.dateTime}</p>
 <p>{event.end?.dateTime}</p>
-<button>onClick={() => setshowDetails(!showDetails)} {showDetails ? "Hide" : "Show"}</button>
+</div>
+
+<div className="eventDetails">
   {showDetails ? (
    <p>{event.description}</p>     
       ) : null}
+      </div>
+      <button>onClick={() => setshowDetails(!showDetails)} {showDetails ? "Hide" : "Show"}</button>
       </li>
     );
   }
