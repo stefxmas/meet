883fb51aebb2718fd25d2cfc8243efb8ccbcec5a
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-10-21 14:35:04 -0400

    enttoend.test

---
+++ src/__tests__/EndToEnd.test.js
@@ -0,0 +1,20 @@
+import puppeteer from 'puppeteer-core';
+
+
+
+describe('<App /> component', () => {
+
+    test("browser" , () => { 
+        (async () => {
+            const browser = await puppeteer.launch(); // Launches the browser
+            const page = await browser.newPage(); // Opens a new tab
+            await page.goto('http://localhost:3000'); // Navigates to https://example.com
+            
+            await browser.close(); // Closes the browser
+          })();
+
+    })
+
+
+    
+});
