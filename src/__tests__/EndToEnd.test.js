107a7f76703929eba25f5e49264f6f8b60d8ca81
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-10-21 14:38:18 -0400

    test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -9,7 +9,7 @@ describe('<App /> component', () => {
             const browser = await puppeteer.launch(); // Launches the browser
             const page = await browser.newPage(); // Opens a new tab
             await page.goto('http://localhost:3000'); // Navigates to https://example.com
-            
+            await page.waitForSelector("#event-list");
             await browser.close(); // Closes the browser
           })();
 
