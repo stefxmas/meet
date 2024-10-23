ea339c2990074c4bdd0509f45d5f86db8a5e524b
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-10-23 13:48:59 -0400

    showHideAnEventDetails & test

--- src/features/showHideAnEventsDetails.feature
+++ src/features/showHideAnEventsDetails.feature
@@ -1,5 +1,16 @@
 Feature: Show/Hide Event Details:
-Scenario: an event element is collapsed by default.
-Given user hasn't selected an event
-When user views the list of events
 
+Scenario:  An event element is collapsed by default
+Given user is on the app
+When the app display list of events
+Then the user should see the event element collapsed by default.
+  
+Scenario: User can expand an event to see details
+Given the app is and events details are hidden
+When the user click the show event button 
+Then the user should see the hidden event expand to view.
+
+Scenario: User can collapse an event to hide details.
+Given the event is expanded and it's visible
+When user click then hide event button
+Then user should see the event collape and hidden
---
+++ src/features/showHideAnEventsDetails.test.js
@@ -0,0 +1,73 @@
+import { render, waitFor, within, } from '@testing-library/react';
+import { loadFeature, defineFeature } from "jest-cucumber";
+import App from "../App";
+import userEvent from '@testing-library/user-event';
+import { getEvents } from '../api';
+import Event from '../component/Event';
+
+ 
+const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
+
+defineFeature(feature, (test) => {
+  test('An event element is collapsed by default', ({ given, when, then }) => {    
+    let AppDOM;
+    given('user is on the app', () => {                                                                   
+      AppDOM = render(<App />).container.firstChild;                                                                                            
+    });                                                                                                   
+                                                                                                          
+    when('the app display list of events', async () => {
+      const EventListDOM = AppDOM.querySelector('#event-list');
+      await waitFor(() => {
+        const EventListItem = within(EventListDOM).getAllByRole('listitem');
+        expect(EventListItem).toHaveLength(32);
+      });
+    });
+
+    then('the user should see the event element collapsed by default.', () => {
+      const eventDetails = AppDOM.querySelector('.details');
+      expect(eventDetails).not.toBeInTheDocument();
+    });
+  });
+  
+  test('User can expand an event to see details', ({ given, when, then }) => {
+    let EventComponent;
+    let allEvents;
+    given('the app is and events details are hidden', async () => {
+      allEvents = await getEvents();
+      EventComponent = render(<Event event={allEvents[0]} />);
+      expect(EventComponent.container.querySelector('.details')).not.toBeInTheDocument();
+    });
+
+    when('the user click the show event button', async () => {
+      const user = userEvent.setup();
+      const showDetails = EventComponent.queryByText('Show Details');
+      await user.click(showDetails);
+    });
+
+    then('the user should see the hidden event expand to view.', () => {
+      const eventDetails = EventComponent.container.querySelector('.details');
+      expect(eventDetails).toBeInTheDocument(); 
+
+    });
+  });
+  
+  test('User can collapse an event to hide details.', ({ given, when, then }) => {
+    let EventComponent;
+    let allEvents;
+    given('the event is expanded and it\'s visible', async () => {
+      allEvents = await getEvents();
+      EventComponent = render(<Event event={allEvents[0]} />);
+    });
+
+    when('user click then hide event button', () => {
+      const user = userEvent.setup();
+      const hideDetails = EventComponent.queryByText('Hide Details');  
+      user.click(hideDetails); 
+    });
+
+    then('user should see the event collape and hidden', () => { 
+      const eventDetails = EventComponent.container.querySelector('.details');
+      expect(eventDetails).not.toBeInTheDocument(); 
+    });
+}); 
+}); 
