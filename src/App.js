88d237c8a1194433a08038eb497ced90362334dc
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-10-28 13:40:25 -0400

    app.js/css & Alert. js
    added Alert.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import './App.css';
 import CitySearch from './component/CitySearch';
 import NumberOfEvents from './component/NumberOfEvents';
 import EventList from './component/EventList';
+import { InfoAlert } from './components/Alert';
 
 const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
-
-
+  const [infoAlert, setInfoAlert] = useState("");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
     console.log("All Events: ", allEvents);
     const filteredEvents = currentCity === "See all cities" ?
       allEvents :
-      allEvents.filter(event => event.location === currentCity)
-      console.log("Filtered Events: ", filteredEvents);
+      allEvents.filter(event => event.location === currentCity);
+    console.log("Filtered Events: ", filteredEvents);
     setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
   }
@@ -30,12 +30,14 @@ const App = () => {
 
   return (
     <div className="App">
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents currentNOE={currentNOE} setCurrentNOE={setCurrentNOE}/>
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+      </div>
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert}/> 
+      <NumberOfEvents setCurrentNOE={setCurrentNOE} />
       <EventList events={events} />
     </div>
   );
 }
 
 export default App;
-
