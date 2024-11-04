500097abc242f3d81b96de0bd249477c633b5389
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-11-04 15:04:17 -0500

    alert&numofEvents
    bring back ErrorAlert
    replaced errorMessage with errorAlert

--- src/component/Alert.js
+++ src/component/Alert.js
@@ -42,12 +42,11 @@ class InfoAlert extends Alert {
 }
 
 class ErrorAlert extends Alert {
-    constructor(props) {
-      super(props);
-      this.color = 'blue'; // blue
-      this.bgColor = 'blue'; // light blue
-    }
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(0, 0, 255)'; // blue
+    this.bgColor = 'rgb(220, 220, 255)'; // light blue
   }
-
+}
 
 export {InfoAlert, ErrorAlert};
--- src/component/NumberOfEvents.js
+++ src/component/NumberOfEvents.js
@@ -1,15 +1,15 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
 import React, { useState } from 'react';
 
-const NumberOfEvents = ({currentNOE, setCurrentNOE}) => {
+const NumberOfEvents = ({currentNOE, setCurrentNOE, setErrorAlert}) => {
   const [ numEvents, setNumEvents] = useState(currentNOE);
-  const [errorMessage, setErrorMessage] = useState("");
+  
   const handleInputChanged = (event) => {
     const value = event.target.value;
     if (isNaN(value) || value <= 0) {
-      setErrorMessage("Please enter a number greater than 0");
+      setErrorAlert("Please enter a number greater than 0");
     } else {
-      setErrorMessage(""); // Clear error message if input is valid
+      setErrorAlert(""); // Clear error message if input is valid
       setNumEvents(value);
       setCurrentNOE(value); // Update the number of events in parent component
     }
@@ -26,7 +26,7 @@ const NumberOfEvents = ({currentNOE, setCurrentNOE}) => {
         onChange={handleInputChanged}
       />
 
-        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        {/* {errorMessage && <div className="error-message">{errorMessage}</div>} */}
     </div>
   );
 };
