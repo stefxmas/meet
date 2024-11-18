d9183ba5bcc5c1efc15a1a5899ad860b3ac252f4
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-11-18 13:30:11 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -31,6 +31,7 @@ export const getEvents = async () => {
     removeQuery();
     const url =  "https://de17z49gj7.execute-api.us-east-1.amazonaws.com/dev/api/get-events" + "/" + token;
     const response = await fetch(url);
+    console.log(response)
     const result = await response.json();
     console.log(result)
     if (result) {
